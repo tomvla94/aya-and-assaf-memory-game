ffae95e98ca1225fd79bef77f4a6d28623eafe82
ayachiprut <ayachiprut@94df5ea0-39fc-e1a8-b613-c9d25f8efa69>
2011-08-10 16:58:17 +0000

    new branch

---
+++ branches/C11 Ex02 Aya 021923107 Assaf 036722999/C11_Ex02/MemBL.cs
@@ -0,0 +1,103 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="MemBL.cs">
+// Aya Chiprut 021923107 
+// Assaf Miron 036722999
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace C11_Ex02
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    /// <summary>
+    /// TODO: Update summary.
+    /// </summary>
+    public class MemBL
+    {
+        private MemBoard m_MemoryBoard = new MemBoard();
+        private bool v_GameExists = false;
+        private bool v_GameEnded = false;
+        private int m_CurrentPlayingPlayerIndex;
+        private List<Player> m_players = new List<Player>();
+  
+        public bool IsGameExists
+        {
+            get { return v_GameExists; }
+        }
+
+        public bool IsGameEnd
+        {
+            get { return v_GameEnded; }
+        }
+
+        public MemBoard Board
+        {
+            get { return m_MemoryBoard; }
+        }
+
+        /// <summary>
+        /// Creates the Memory Game Board
+        /// </summary>
+        /// <param name="i_BoardWidth">The Board Width</param>
+        /// <param name="i_BoardHeight">The Board Height</param>
+        public void CreateMemoryGame(int i_BoardWidth, int i_BoardHeight)
+        {
+            this.m_MemoryBoard.BuildBoard(i_BoardWidth, i_BoardHeight);
+        }
+
+        /// <summary>
+        /// Initializes the Memory Board Game.
+        /// Creates The Board and Sets other Important Variables
+        /// </summary>
+        public void InitializeMemoryGame()
+        { 
+            // Create the Memory Game
+            CreateMemoryGame(0, 0);
+
+            // Set who will be the Next Player to Play
+            m_CurrentPlayingPlayerIndex = 0;
+            
+            // Set the Game is Started
+            v_GameExists = true;
+        }
+
+        /// <summary>
+        /// Initialize the Players
+        /// </summary>
+        /// <param name="i_PlayerName">Players Name Array</param>
+        public void InitializePlayers(params string[] i_PlayerName)
+        {
+            if (i_PlayerName.Length == 1)
+            {
+                this.m_players.Add(new Player(i_PlayerName[0], Player.ePlayerType.Human));
+                this.m_players.Add(new Player(Player.ePlayerType.Computer));
+            }
+            else if (i_PlayerName.Length == 2)
+            {
+                this.m_players.Add(new Player(i_PlayerName[0], Player.ePlayerType.Human));
+                this.m_players.Add(new Player(i_PlayerName[1], Player.ePlayerType.Human));
+            }   
+        }
+
+        /// <summary>
+        /// Plays The Current Playing Player Turn
+        /// </summary>
+        public void PlayPlayerTurn() 
+        {
+            // TODO: This is the Do Move Function
+
+            // Get User Input about Selected Squares
+            // Verify Valid Input
+            // Throw Error Messages if Input not Valid
+            // Score The Player if Player is Correct
+            // Change Turns
+        }
+
+        public string PrintGameBoard()
+        {
+            return m_MemoryBoard.PrintBoard();
+        }
+    }
+}
---
+++ branches/C11 Ex02 Aya 021923107 Assaf 036722999/C11_Ex02/MemoryGame.cs
@@ -0,0 +1,162 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="MemoryGame.cs">
+// Aya Chiprut 021923107 
+// Assaf Miron 036722999
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace C11_Ex02
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    using Ex02.ConsoleUtils;
+
+    /// <summary>
+    /// User Interface
+    /// </summary>
+    public class MemoryGame
+    {
+        int m_NumberOfHumanPlayers = 0;
+        MemBL m_MemoryLogic = new MemBL();
+
+        /// <summary>
+        /// Get information from the user
+        /// </summary>
+        public void InitGame()
+        {
+            // Get Player Name
+            // Get number of Players in the Game
+            GetPlayersInfoFromUser();
+            GetBoardInfoFromUser();
+        }
+
+        private void GetBoardInfoFromUser()
+        {
+            bool isLegal;
+            string userInput;
+            int height = 0;
+            int width = 0;
+            // TODO: Let the User Choose Other Sizes in the Same Format
+            // Use Check Input From Board (or Something) 
+            Console.WriteLine("please choose the board size: (4x4, 4X6, 6X4, 6X6)");
+            do
+            {
+                userInput = getUserInput();
+                if (userInput.Equals("4X4", StringComparison.OrdinalIgnoreCase))
+                {
+                    isLegal = true;
+                    width = 4;
+                    height = 4;
+                }
+                else if (userInput.Equals("4X6", StringComparison.OrdinalIgnoreCase))
+                {
+                    isLegal = true;
+                    width = 4;
+                    height = 6;
+                }
+                else if (userInput.Equals("6X4", StringComparison.OrdinalIgnoreCase))
+                {
+                    isLegal = true;
+                    width = 6;
+                    height = 4;
+                }
+                else if (userInput.Equals("6X6", StringComparison.OrdinalIgnoreCase))
+                {
+                    isLegal = true;
+                    width = 6;
+                    height = 6;
+                }
+                else
+                {
+                    isLegal = false;
+                    Console.WriteLine("wrong input, please choose the board size: (4x4, 4X6, 6X4, 6X6)");
+                }
+            } 
+            while (!isLegal);
+
+            m_MemoryLogic.CreateMemoryGame(width, height);
+        }
+
+        private void GetPlayersInfoFromUser()
+        {
+            int numOfHumanPlayers = 0;
+            string userInput;
+            bool isLegal;
+            Console.WriteLine("Please type your name: ");
+            string playerName = Console.ReadLine();
+            Console.WriteLine(playerName + ", Do you want to play against the computer? Y/N: ");
+
+            do
+            {
+                userInput = getUserInput();
+                if (userInput.Equals("y", StringComparison.OrdinalIgnoreCase))
+                {
+                    isLegal = true;
+                    numOfHumanPlayers = 1;
+                }
+                else if (userInput.Equals("n", StringComparison.OrdinalIgnoreCase))
+                {
+                    isLegal = true;
+                    numOfHumanPlayers = 2;
+                }  
+                else
+                {
+                    isLegal = false;
+                    Console.WriteLine("wrong input, do you want to play against the computer? Y/N: ");
+                }
+            } while (!isLegal);
+
+            if (numOfHumanPlayers == 2)
+            {
+                Console.WriteLine("What is the second player name: ");
+                string secondPlayerName = Console.ReadLine();
+
+                // Construct Players (Both Humans)
+                m_MemoryLogic.InitializePlayers(playerName, secondPlayerName);
+            }
+            else
+            {
+                // Construct Players (Human and Computer)
+                m_MemoryLogic.InitializePlayers(playerName);
+            }
+        }
+
+        /// <summary>
+        /// Gets the user input, if the user input is 'Q' - Exiting the application
+        /// </summary>
+        /// <returns></returns>
+        private string getUserInput()
+        {
+            string userInput = Console.ReadLine();
+
+            if (userInput.Equals("q", StringComparison.OrdinalIgnoreCase))
+            {
+                Console.WriteLine("You chose to quit the game.");
+                endGame();
+            }
+
+            return userInput;
+        }
+
+        private static void endGame()
+        {
+            throw new NotImplementedException();
+        }
+
+       
+        /// <summary>
+        /// Gets the Number of Players From Console
+        /// </summary>
+        /// <returns></returns>
+        public int GetNumberOfPlayers()
+        {
+            return 0;
+        }
+
+        public string PrintGameBoard()
+        {
+            return m_MemoryLogic.PrintGameBoard();
+        }
+    }
+}
