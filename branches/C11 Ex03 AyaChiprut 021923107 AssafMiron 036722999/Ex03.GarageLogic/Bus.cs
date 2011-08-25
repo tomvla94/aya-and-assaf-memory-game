48192bcfeb17513eb9411d41b938629ab8894b23
Assaf.Miron@gmail.com <Assaf.Miron@gmail.com@94df5ea0-39fc-e1a8-b613-c9d25f8efa69>
2011-08-25 21:35:00 +0000

    Removed old not necessary Classes Updated Some Functions Mainly the GetDetails, SetPropertiesFromInput and GetPropertiesForInput. Need to Create a hierarchy of GetDetails Functions on all Objects. Need to restore Sub Classes of Fueled/Electric Vehicles.

--- branches/C11 Ex03 AyaChiprut 021923107 AssafMiron 036722999/Ex03.GarageLogic/Bus.cs
+++ branches/C11 Ex03 AyaChiprut 021923107 AssafMiron 036722999/Ex03.GarageLogic/Bus.cs
@@ -22,7 +22,21 @@ namespace Ex03.GarageLogic
         private int m_MaxAllowedAmountOfPassengers;
         private bool v_HasGuideSeat;
 
-        protected Bus(
+        public Bus(
+            string i_Model,
+            string i_LicenseNumber,
+            List<Wheel> i_Wheels,
+            Engine i_EngineType)
+            : base(i_Model, i_LicenseNumber, k_NumOfWheels, i_Wheels)
+        {
+            m_Engine = i_EngineType;
+            m_PropertiesForInput = new List<string>();
+
+            m_PropertiesForInput.Add("Maximum Allowed Number of Passengers");
+            m_PropertiesForInput.Add("Guide Seat");
+        }
+
+        public Bus(
             string i_Model,
             string i_LicenseNumber,
             List<Wheel> i_Wheels,
@@ -42,6 +56,31 @@ namespace Ex03.GarageLogic
         public bool HasGuideSeat
         {
             get { return v_HasGuideSeat; }
+        }
+
+        public override List<string> GetPropertiesForInput()
+        {
+            return m_PropertiesForInput;
+        }
+
+        public override void SetPropertiesFromInput(List<string> i_PropertiesFromUser)
+        {
+            // Get First Parameter - The Number of Allowed Passengers
+            bool tryParse = int.TryParse(i_PropertiesFromUser[0], out m_MaxAllowedAmountOfPassengers);
+            i_PropertiesFromUser.RemoveAt(0);
+
+            // Get Second Parameter - If the Bus has a Guid seat or not
+            v_HasGuideSeat = bool.Parse(i_PropertiesFromUser[0]);
+            i_PropertiesFromUser.RemoveAt(0);
+        }
+
+        public override string GetDetails()
+        {
+            string retDetails = string.Format("Maximum Allowed Passengers: {0}{3}"
+                         + "The Bus Has a Guide Seat: {1}{3}"
+                          , m_MaxAllowedAmountOfPassengers.ToString(), v_HasGuideSeat.ToString(), Environment.NewLine);
+
+            return retDetails;
         }
     }
 }
---
+++ branches/C11 Ex03 AyaChiprut 021923107 AssafMiron 036722999/Ex03.GarageLogic/FueledCar.cs
@@ -0,0 +1,54 @@
+﻿﻿// -----------------------------------------------------------------------
+// <copyright file="FueledCar.cs">
+// Aya Chiprut 021923107 
+// Assaf Miron 036722999
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace Ex03.GarageLogic
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    /// <summary>
+    /// TODO: Update summary.
+    /// </summary>
+    public class FueledCar
+    {
+        private Car m_Vehicle;
+        private const float k_MaxFuelLiters = 45;
+        private const FuelTypedVehicle.eFuelType k_FuelType = FuelTypedVehicle.eFuelType.Octan95;
+        protected List<string> m_PropertiesForInput;
+
+        public FueledCar(
+            string i_Model,
+            string i_LicenseNumber,
+            string i_WheelManufacturer,
+            float i_WheelCurrentAirPressure,
+            Car.eCarColor i_CarColor,
+            int i_NumOfDoors,
+            float i_RemainingFuelLiters)
+        {
+            List<Wheel> wheels = new List<Wheel>(); // TODO: Complete Wheels List
+            m_Vehicle = new Car(i_Model, i_LicenseNumber, wheels, new FuelTypedVehicle(k_FuelType, k_MaxFuelLiters, i_RemainingFuelLiters));
+
+            m_PropertiesForInput = new List<string>();
+        }
+
+        public override List<string> GetPropertiesForInput()
+        {
+            return m_PropertiesForInput;
+        }
+
+        public override void SetPropertiesFromInput(List<string> i_PropertiesFromUser)
+        {
+            m_Vehicle.SetPropertiesFromInput(i_PropertiesFromUser);
+        }
+
+        public override string GetDetails()
+        {
+            return m_Vehicle.GetDetails();
+        }
+    }
+}
