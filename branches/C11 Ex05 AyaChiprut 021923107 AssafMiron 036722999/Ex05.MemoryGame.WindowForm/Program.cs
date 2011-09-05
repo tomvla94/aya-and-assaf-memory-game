fd0b3450c063f4018e7cf6dd93bedcf62d796a06
Assaf.Miron@gmail.com <Assaf.Miron@gmail.com@94df5ea0-39fc-e1a8-b613-c9d25f8efa69>
2011-09-05 17:05:45 +0000

    Ex05.MemoryGame.WindowForm

---
+++ branches/C11 Ex05 AyaChiprut 021923107 AssafMiron 036722999/Ex05.MemoryGame.WindowForm/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ex05.MemoryGame.WindowForm
+{
+    class Program
+    {
+        public static void Main()
+        {
+            SettingsForm memSettings = new SettingsForm();
+
+            memSettings.ShowDialog();
+        }
+    }
+}
